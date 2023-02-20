3fc874a36b80bf31d2f34127af3f1cd30374ca6b
Michael Buholzer <michael.buholzer@outlook.com>
2023-02-20 11:23:00 +0100

    add project files

---
+++ CSA-FS23/Uebung01-HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uebung01_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMessage("Hello, World!", false);
+            PrintMessage("Hello, World!", true);
+        }
+
+        /// <summary>
+        /// Gibt eine Meldung auf dem Bildschirm aus
+        /// </summary>
+        /// <param name="message">Anzuzeigende Nachrichte</param>
+        /// <param name="toUpper">Ob alles in GROSS oder normalen Buchstaben</param>
+        private static void PrintMessage(string message, bool toUpper)
+        {
+            if (toUpper)
+            {
+                message = message.ToUpper();
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
